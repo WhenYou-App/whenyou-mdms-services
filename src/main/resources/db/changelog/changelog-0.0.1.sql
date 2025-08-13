a1bde705cb0c1840e92c0d09268c330486a44359
FentoCM <fentosonu120@gmail.com>
2025-08-13 15:41:13 +0530

    changes in change log

--- src/main/resources/db/changelog/changelog-0.0.1.sql
+++ src/main/resources/db/changelog/changelog-0.0.1.sql
@@ -1,10 +1,16 @@
--- CREATE TABLE security_login (
---        id                             UUID NOT NULL PRIMARY KEY,
---        phone_number                   VARCHAR(20) NOT NULL,
---        password                       VARCHAR(255) NOT NULL,
---        created_at                     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---        created_by                     UUID,
---        modified_by                    UUID,
---        modified_date                  TIMESTAMP,
---        last_login                     TIMESTAMP
---);
+-- Table: m_districts
+CREATE TABLE m_districts (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(150) NOT NULL,
+    name_in_local VARCHAR(150),
+    status BOOLEAN DEFAULT TRUE
+);
+
+-- Table: m_pincode
+CREATE TABLE m_pincode (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(150) NOT NULL,
+    name_in_local VARCHAR(150),
+    pincode VARCHAR(10) NOT NULL,
+    status BOOLEAN DEFAULT TRUE
+);
